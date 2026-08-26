da081fe0e4adc9d09cde73cc9505ef488521668f
Mohamed Rabei <mo7amed.rabei14@gmail.com>
2026-08-26 09:08:17 +0300

    Fix CORS issue

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app = FastAPI(title="EOC System", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], # 💡 شيلنا النجمة وحطينا روابط الفرونت إند صريحة
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
